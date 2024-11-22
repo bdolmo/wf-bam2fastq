ea0cd0622cea85bffe3024d9ec4632686e5272ca
bdolmo <bdelolmo@gencardio.com>
2024-11-22 14:29:50 +0100

    removed temporary files

--- work/2d/45aa95d6f71c111c0a6e6dc0d1195c/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -euo pipefail
-echo "Processing S188428.bam with windows.1000kb.bed..."
-fragment_extractor S188428.bam windows.1000kb.bed         output/S188428_fragments.bed         output/S188428_fragments.wig
